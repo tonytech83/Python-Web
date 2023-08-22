42780cb27b97e9d4e08ee98b956cf68e797d8dc3
Anton Petrov <a.petrov.83@gmail.com>
2023-08-22 15:30:28 +0300

    add two PostgreSQL queries

---
+++ 01_PostgreSQL_Introduction/create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE people (
+	id SERIAL PRIMARY KEY,
+	email VARCHAR(50) NOT NULL,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50)
+);
---
+++ 01_PostgreSQL_Introduction/insert_into_table.sql
@@ -0,0 +1,12 @@
+INSERT
+	INTO
+	people (email,
+	first_name,
+	last_name)
+VALUES 
+('example@example.com',
+'Georgi',
+'Ivanov'),
+('penka@abv.bg',
+'Penka',
+'TheClasher')
