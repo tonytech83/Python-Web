2b36c78aa043d5319f1e332db9625a92d8ab4f03
Anton Petrov <a.petrov.83@gmail.com>
2023-08-23 10:43:33 +0300

    added queries

---
+++ 01_PostgreSQL_Introduction/filtering_selection.sql
@@ -0,0 +1,16 @@
+SELECT
+	last_name
+FROM
+	people p
+WHERE .id = 1;
+
+SELECT
+	id,
+	first_name,
+	last_name
+FROM
+	people p
+WHERE
+	p.id > 1
+	AND p.id < 3;
+	
---
+++ 01_PostgreSQL_Introduction/limit_select.sql
@@ -0,0 +1,6 @@
+SELECT
+	first_name ,
+	last_name
+FROM
+	people p
+LIMIT 1;
