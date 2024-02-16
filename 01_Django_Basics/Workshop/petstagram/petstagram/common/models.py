a56b22da976a9c84460027694e5c0ca672d25abf
Anton Petrov <a.petrov.83@gmail.com>
2024-02-16 09:52:33 +0200

    added Workshop: Part 1

--- 01_Django_Basics/Workshop/petstagram/petstagram/common/models.py
+++ 01_Django_Basics/Workshop/petstagram/petstagram/common/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from petstagram.photos.models import Photo
+
+
+class PhotoComment(models.Model):
+    MAX_TEXT_LENGTH = 300
+
+    text = models.TextField(
+        max_length=MAX_TEXT_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    date_time_of_publication = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    photo = models.ForeignKey(
+        to=Photo,
+        on_delete=models.DO_NOTHING,
+    )
+
+
+class PhotoLike(models.Model):
+    to_photo = models.ForeignKey(
+        to=Photo,
+        on_delete=models.DO_NOTHING,
+    )
