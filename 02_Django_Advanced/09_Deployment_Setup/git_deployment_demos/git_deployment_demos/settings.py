5c7f191896150c4bd565050c6308804c40c489b3
tonytech83 <a.petrov.83@gmail.com>
2024-04-04 13:35:34 +0300

    Added demos for deployment

--- 02_Django_Advanced/09_Deployment_Setup/git_deployment_demos/git_deployment_demos/settings.py
+++ 02_Django_Advanced/09_Deployment_Setup/git_deployment_demos/git_deployment_demos/settings.py
@@ -13,7 +13,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'kubernetes.docker.internal',
+    'localhost',
+]
 
 # Application definition
 
@@ -100,6 +103,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+]
+
+STATIC_ROOT = BASE_DIR / 'static_files'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
